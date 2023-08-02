3e6a438bb3557da1b900d0a62ee0ae458609365e
Easop Lee <generalroboticslab@pse-bc298-dt02.egr.duke.edu>
2023-08-02 15:34:20 -0400

    wrist movement idea to collect scene data

--- panda_base.py
+++ panda_base.py
@@ -9,10 +9,12 @@ class PandaGripperD435(object):
 
         self.hand_idx = 8
         self.writst_camera_idx = 12
-
+    
         start_pos = [0, 0, 0.001]
         start_orientation = self.bullet_client.getQuaternionFromEuler([0, 0, 0])
         self.panda_id = self.bullet_client.loadURDF("assets/franka_panda/panda.urdf", start_pos, start_orientation, useFixedBase=True)
+        self._movable_joints = self.get_movable_joints()
+
         self.reset()
 
     def reset(self):
@@ -21,70 +23,70 @@ class PandaGripperD435(object):
             self.bullet_client.resetJointState(self.panda_id, i, initial_pos[i])
 
     # returns id's of all the movable robot joints
-    # def get_movable_joints(self):
-    #     movable_joint_ids = []
-    #     for idx in self._all_joints_ids:
-    #         joint_info = self.bullet_client.getJointInfo(self.robot_id, idx)
-    #         q_index = joint_info[3]
-    #         if q_index > -1:
-    #             movable_joint_ids.append(idx)
+    def get_movable_joints(self):
+        movable_joint_ids = []
+        for idx in range(self.bullet_client.getNumJoints(self.panda_id)):
+            joint_info = self.bullet_client.getJointInfo(self.panda_id, idx)
+            q_index = joint_info[3]
+            if q_index > -1:
+                movable_joint_ids.append(idx)
 
-    #     return movable_joint_ids
+        return movable_joint_ids
 
-    # def get_joint_state(self, joint_id=None):
-    #     """
-    #     :return: joint positions, velocity, reaction forces, joint efforts as given from bullet physics
-    #     :rtype: [np.ndarray, np.ndarray, np.ndarray, np.ndarray]
-    #     """
-    #     if joint_id is None:
+    def get_joint_state(self, joint_id=None):
+        """
+        :return: joint positions, velocity, reaction forces, joint efforts as given from bullet physics
+        :rtype: [np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+        """
+        if joint_id is None:
 
-    #         joint_angles = []
-    #         joint_velocities = []
-    #         joint_reaction_forces = []
-    #         joint_efforts = []
+            joint_angles = []
+            joint_velocities = []
+            joint_reaction_forces = []
+            joint_efforts = []
 
-    #         for idx in self._movable_joints:
-    #             joint_state = self.bullet_client.getJointState(self.robot_id, idx)
-    #             joint_angles.append(joint_state[0])
-    #             joint_velocities.append(joint_state[1])
-    #             joint_reaction_forces.append(joint_state[2])
-    #             joint_efforts.append(joint_state[3])
+            for idx in self._movable_joints:
+                joint_state = self.bullet_client.getJointState(self.panda_id, idx)
+                joint_angles.append(joint_state[0])
+                joint_velocities.append(joint_state[1])
+                joint_reaction_forces.append(joint_state[2])
+                joint_efforts.append(joint_state[3])
 
-    #         return np.array(joint_angles), np.array(joint_velocities), np.array(joint_reaction_forces), np.array(joint_efforts)
+            return np.array(joint_angles), np.array(joint_velocities), np.array(joint_reaction_forces), np.array(joint_efforts)
 
-    #     else:
-    #         joint_state = self.bullet_client.getJointState(self.robot_id, joint_id)
-    #         joint_angle = joint_state[0]
-    #         joint_velocity = joint_state[1]
-    #         joint_reaction_forces = joint_state[2]
-    #         joint_effort = joint_state[3]
+        else:
+            joint_state = self.bullet_client.getJointState(self.panda_id, joint_id)
+            joint_angle = joint_state[0]
+            joint_velocity = joint_state[1]
+            joint_reaction_forces = joint_state[2]
+            joint_effort = joint_state[3]
 
-    #         return joint_angle, joint_velocity, np.array(joint_reaction_forces), joint_effort
+            return joint_angle, joint_velocity, np.array(joint_reaction_forces), joint_effort
 
-    # def get_link_pose(self, link_id):
-    #     """
-    #     :return: Pose of link (Cartesian positionof center of mass, Cartesian orientation of center of mass in quaternion [x,y,z,w]) 
-    #     :rtype: [np.ndarray, np.quaternion]
-    #     :param link_id: optional parameter to specify the link id. If not provided, will return pose of end-effector
-    #     :type link_id: int
-    #     """
+    def get_link_pose(self, link_id):
+        """
+        :return: Pose of link (Cartesian positionof center of mass, Cartesian orientation of center of mass in quaternion [x,y,z,w]) 
+        :rtype: [np.ndarray, np.quaternion]
+        :param link_id: optional parameter to specify the link id. If not provided, will return pose of end-effector
+        :type link_id: int
+        """
 
-    #     link_state = self.bullet_client.getLinkState(self.robot_id, link_id)
-    #     pos = np.asarray(link_state[0])
-    #     ori = np.array([link_state[1][3], link_state[1][0], link_state[1][1], link_state[1][2]])  # hamilton convention
+        link_state = self.bullet_client.getLinkState(self.panda_id, link_id)
+        pos = np.asarray(link_state[0])
+        ori = np.array([link_state[1][3], link_state[1][0], link_state[1][1], link_state[1][2]])  # hamilton convention
 
-    #     return pos, ori
+        return pos, ori
 
-    # def get_ee_pose(self):
-    #     """
-    #     :return: end-effector pose of this robot in the format (position,orientation)
-    #     .. note: orientation is a quaternion following Hamilton convention, i.e. (w, x, y, z)
-    #     """
-    #     return self.get_link_pose(link_id=self.ee_idx)
+    def get_hand_pose(self):
+        """
+        :return: end-effector pose of this robot in the format (position,orientation)
+        .. note: orientation is a quaternion following Hamilton convention, i.e. (w, x, y, z)
+        """
+        return self.get_link_pose(link_id=self.hand_idx)
     
-    # def get_camera_pos(self):
-    #     """
-    #     :return: wrist mounted camera pose on this robot in the format (position,orientation)
-    #     .. note: orientation is a quaternion following Hamilton convention, i.e. (w, x, y, z)
-    #     """
-    #     return self.get_camera_pos(link_id=self.writst_camera_idx)
+    def get_camera_pos(self):
+        """
+        :return: wrist mounted camera pose on this robot in the format (position,orientation)
+        .. note: orientation is a quaternion following Hamilton convention, i.e. (w, x, y, z)
+        """
+        return self.get_camera_pos(link_id=self.writst_camera_idx)
--- panda_w_camera.py
+++ panda_w_camera.py
@@ -1,6 +1,7 @@
 from panda_base import PandaGripperD435
 from pybullet_base import PyBulletBase
 import numpy as np
+import time
 
 class panda_camera(PyBulletBase):
     def __init__(self, gui_enabled):
@@ -23,15 +24,38 @@ class panda_camera(PyBulletBase):
         camera_vector = rot_matrix.dot(init_camera_vector)
         up_vector = rot_matrix.dot(init_up_vector)
         view_matrix = self.bullet_client.computeViewMatrix(com_p, com_p + 0.5 * camera_vector, up_vector)
-        self.bullet_client.addUserDebugLine(com_p, com_p + 0.5 * camera_vector, [0, 0, 1], 3, 5)
+        self.cam_lookat = com_p + 0.5 * camera_vector
+        self.bullet_client.addUserDebugLine(com_p, self.cam_lookat, [0, 0, 1], 3, 5)
 
         fov, aspect, nearplane, farplane = 60, 1.0, 0.01, 100
         projection_matrix = self.bullet_client.computeProjectionMatrixFOV(fov, aspect, nearplane, farplane)
         img = self.bullet_client.getCameraImage(256, 256, view_matrix, projection_matrix)
-        return img
+        return img, self.cam_lookat
+    
+    def move_wrist(self):
+        curr_speed = 0.1
+        turn = 0
+        while True:
+            self.bullet_client.stepSimulation()
+            self.panda_camera()
+            curr_pos = self.panda.get_joint_state(4)[0]
+            if self.cam_lookat[2] > 0.65 and turn == 0:
+                curr_speed *= -1  
+                turn += 1
+            if self.cam_lookat[2] > 0.75 and turn == 1:
+                break
+            
+            theta = curr_pos + curr_speed
+            joint_angle = theta  
+            
+            # Set the joint position to move the wrist
+            self.bullet_client.setJointMotorControl2(self.panda.panda_id, 4, self.bullet_client.POSITION_CONTROL, targetPosition=joint_angle)
 
 if __name__ == '__main__':
     panda = panda_camera(gui_enabled=True)
+    panda.move_wrist()
+    
     while True:
         panda.bullet_client.stepSimulation()
         panda.panda_camera()
+        panda.panda.reset()
